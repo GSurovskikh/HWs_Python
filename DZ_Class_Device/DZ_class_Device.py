333d7d41add3aeda4756910a99b6b49cd06cb60e
Gennadii <gena27-bel31@yandex.ru>
2022-02-26 13:27:03 +0300

    Create class Device

---
+++ DZ_Class_Device/DZ_class_Device.py
@@ -0,0 +1,9 @@
+class Device:
+    def __init__(self, price, manufacturer, color, height,weight):
+        self.price = float(price)
+        self.manufacturer = manufacturer
+        self.color = color
+        self.height = height
+        self.weight = weight
+
+
