bc3ac05343122124cf2101ede4533d381871e278
Gennadii <gena27-bel31@yandex.ru>
2022-02-26 13:42:31 +0300

    Update DZ_class_Device.py
    Create Classes CoffeeMachine,Blender,MeatGrinder using Inheritance

--- DZ_Class_Device/DZ_class_Device.py
+++ DZ_Class_Device/DZ_class_Device.py
@@ -1,9 +1,24 @@
 class Device:
-    def __init__(self, price, manufacturer, color, height,weight):
+    def __init__(self, price, manufacturer, color, height,weight,type):
         self.price = float(price)
         self.manufacturer = manufacturer
         self.color = color
         self.height = height
         self.weight = weight
+        self.type = type
+
+class CoffeMachine(Device):
+    def __init__(self,*args, volume,type_coffee):
+        super(CoffeMachine, self).__init__(*args)
+
+
+class Blender(Device):
+    def __init__(self,*args,volume,power):
+        super(Blender, self).__init__(*args)
+
+
+class MeatGrinder(Device):
+    def __init__(self,*args,type_meat):
+        super(MeatGrinder, self).__init__(*args)
 
 
