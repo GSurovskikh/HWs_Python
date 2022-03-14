7cc1fe99ff3402d98feac1ee95f71d51633b34ac
Gennadii <gena27-bel31@yandex.ru>
2022-03-14 20:25:31 +0300

    Доработал функцию изменения числа

--- DZ_change_number/DZ_change_number.py
+++ DZ_change_number/DZ_change_number.py
@@ -5,7 +5,7 @@ class Changenum(tk.Frame): #Цифра не изменяется
     def __init__(self,*args,**kwargs):
         super(Changenum, self).__init__(*args,**kwargs)
         self.entry_text = tk.StringVar()
-        self.entry = tk.Entry(self,text=0)
+        self.entry = tk.Entry(self,textvariable=self.entry_text)
         self.entry.grid(column=1,columnspan=2, row=2)
         btn_add = tk.Button(self, text="+")
         btn_add.grid(row=2)
